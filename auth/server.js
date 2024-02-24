8c76f2f486103542845331b09c1a99870b113931
Aman Gupta <eagleax7@gmail.com>
2024-02-24 22:01:20 +0530

    db-and-prisma-setup

--- auth/server.js
+++ auth/server.js
@@ -1,7 +1,14 @@
 import express from "express";
-
+import cors from "cors";
+import bodyParser from "body-parser";
 const app = express();
 const PORT = process.env.PORT || 8000
+
+// Middlewares
+app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("It works")
