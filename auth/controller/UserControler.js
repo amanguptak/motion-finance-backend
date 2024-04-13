b9c19cf5221e39e773c1ac1813782d447ae6b23b
Aman Gupta <eagleax7@gmail.com>
2024-04-13 20:48:54 +0530

    updated user api

--- auth/controller/UserControler.js
+++ auth/controller/UserControler.js
@@ -51,7 +51,20 @@ class UserController {
         if (!req.user || !req.user.email) {
           return res.status(404).json({ message: "Email not found" });
         }
-  
+        const email = req.user.email;
+        const user = await prisma.user.findUnique({
+            where: { email: email }
+        });
+        
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+      }
+      const updatedUser = await prisma.user.update({
+        where: { email: email },
+        data: req.body
+    });
+
+      res.json({ user: updatedUser });
 
       }catch(error){
         res.status(500).json({ message: error.message});
